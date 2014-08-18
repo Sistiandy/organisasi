3d1c82acf30e436a268196ca681619d2291e566f
Sistiandy <sistiandy@artikulpi.com>
2014-08-18 16:06:53 +0700

    Penambahan produk untuk organisasi

---
+++ application/views/main.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Swadaya Berkarya</title>
+
+        <!--CSS From Foundation -->
+        <link rel="stylesheet" href="<?php echo base_url('media/css/normalize.css') ?>">
+        <link rel="stylesheet" href="<?php echo base_url('media/css/foundation.min.css') ?>">
+
+        <!-- Ionicon -->
+        <link rel="stylesheet" href="<?php echo base_url('media/css/ionicons.min.css') ?>">
+
+        <!-- Custom CSS -->
+        <!--<link rel="stylesheet" href="css/app.css"> -->
+
+        <script src="<?php echo base_url('media/js/modernizr.js') ?>"></script>
+
+    </head>
+    <body>
+
+        <div class="antialiased">
+            <div class="off-canvas-wrap">
+                <div class="small-11small-centered columns">
+                    <div class="row">
+                        <ul class="example-orbit" data-orbit data-options="bullets:false;">
+                            <li>
+                                <img src="<?php echo base_url('uploads/satelite-orbit.jpg'); ?>" alt="slide 1" />
+                                <div class="orbit-caption">
+                                    Caption One.
+                                </div>
+                            </li>
+                            <li class="active">
+                                <img src="<?php echo base_url('uploads/andromeda-orbit.jpg'); ?>" alt="slide 2" />
+                                <div class="orbit-caption">
+                                    Caption Two.
+                                </div>
+                            </li>
+                            <li>
+                                <img src="<?php echo base_url('uploads/launch-orbit.jpg'); ?>" alt="slide 3" />
+                                <div class="orbit-caption">
+                                    Caption Three.
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="icon-bar four-up">
+                            <a class="item">
+                                <i class="ion-ios7-home"></i>
+                                <label>Beranda</label>
+                            </a>
+                            <a class="item">
+                                <i class="ion-images"></i>
+                                <label>Galeri Foto</label>
+                            </a>
+                            <a class="item">
+                                <i class="ion-ios7-people"></i>
+                                <label>Anggota</label>
+                            </a>
+                            <a class="item">
+                                <i class="ion-ribbon-a"></i>
+                                <label>Kegiatan</label>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <h2>Tentang "Swadaya Berkarya"</h2>
+                        <p>Artikulpi adalah lembaga pengembang TIK (teknologi informasi dan komunikasi) untuk pembangunan (ICT for Development). Artikulpi telah memulai aktvitasnya sejak tahun 2009 yang bekerjasama dengan berbagai lembaga baik di level nasional maupun internasional. Artikulpi adalah akronim dari falsafah hidup dari masyarakat Jawa yaitu Atine Resik, Tumindak Becik, Uripe Apik (Hati yang bersih, Perilaku yang baik, Hidup pun menjadi bagus). Artikulpi berpandangan bahwa bisnis harus berdasarkan hati dan moral. Bisnis tidak cuma persoalan keuntungan, namun sangat penting mempertimbangkan apakah bisnis yang dijalankan bermanfaat untuk orang banyak ataukah tidak. Hal ini pula yang mendasari produk dan layanan jasa yang diberikan oleh Artikulpi fokus pada hal-hal yang berkaitan dengan pembangunan yang berdampak pada banyak orang.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- body content here -->
+
+        <script src="<?php echo base_url('media/js/jquery-2.1.1.min.js') ?>"></script>
+        <script src="<?php echo base_url('media/js/jquery.js') ?>"></script>
+        <script src="<?php echo base_url('media/js/foundation.min.js') ?>"></script>
+        <script>
+                            $(document).foundation();
+        </script>
+    </body>
+</html>
